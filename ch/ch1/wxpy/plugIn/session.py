5f7bb9aa227ec46c89f793f592f3c90e9cd50603
ynsun <sunyanan@odc.cmbchina.cn>
2019-05-24 08:52:32 +0800

    send message use message.chat

--- ch/ch1/wxpy/plugIn/session.py
+++ ch/ch1/wxpy/plugIn/session.py
@@ -5,7 +5,6 @@ import json
 import dateutil.parser
 from datetime import datetime
 from restHelper import RestTemplate
-from sendHelper import WXSender
 
 
 class Session:
@@ -15,7 +14,6 @@ class Session:
         self.sessionApi = 'https://api.trello.com/1'
         self.groups = groups
         self.reg = 'session'
-        self.sender = WXSender()
 
     def start(self):
         print 'start session bot'
@@ -24,15 +22,15 @@ class Session:
     def monitor(self, msg):
         print msg.text
         if re.search('session-list$', msg.text):
-            self.handle_sessions()
+            self.handle_sessions(msg.chat)
         elif re.search('session-apply', msg.text):
             self.apply_session(msg)
         else:
             print 'session not support'
-            self.sender.send(self.groups, u'支持指令格式：\n 1. session-list \n 2. session-apply-名称-时间')
+            msg.chat.send(u'支持指令格式：\n 1. session-list \n 2. session-apply-名称-时间')
         return
 
-    def handle_sessions(self):
+    def handle_sessions(self, chat):
         print 'start get sessions'
         cards = self.get_sessions()
         print 'get session success'
@@ -41,7 +39,7 @@ class Session:
             response = response + u'名字：%s, 时间：%s \n' % (
                 card['name'], dateutil.parser.parse(card['due']).strftime('%Y-%m-%d'))
         print response
-        return self.sender.send(self.groups, response)
+        return chat.send(response)
 
     def apply_session(self, msg):
         print 'start apply session'
@@ -53,10 +51,10 @@ class Session:
             sender = msg.member
             print date
             card = self.apply_sessions(name, date, sender)
-            self.sender.send(self.groups, u'报名成功, 如需修改请去' + card['url'])
+            msg.chat.send(u'报名成功, 如需修改请去' + card['url'])
         except Exception as e:
             print e.message
-            self.sender.send(self.groups, u'报名失败，请输入正确格式session-apply-名字-时间(YYYYMMDD)')
+            msg.chat.send(self.groups, u'报名失败，请输入正确格式session-apply-名字-时间(YYYYMMDD)')
         return
 
     def get_sessions(self):
